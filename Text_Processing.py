4de7e63459ef4ec6faba185fefeca4b8b476d804
Ghadeer Al Jufout <ghadeer1112@yahoo.com>
2024-10-08 09:53:10 -0700

    Tokenizing Process
    * Reading a character by character
    * Checking if it's alphanum
    * Append it to a list
    * Join the string of characters
    * Normalize the string

--- Text_Processing.py
+++ Text_Processing.py
@@ -3,12 +3,22 @@ import os # For some error checks
 
 def tokenize(file_name): # creating a tokenizer from scratch
     token_frequency = {} # A dictionary to record the frequency of each token
+    current_token = [] # This list will be used to store the alphanum characters
+
     try:
-        with open(file_name, 'r', encoding = 'utf-8') as file: # Opening the file in read mode
-                 #Token logic goes here                                     # Using utf-8 encoding
-            pass
+        with open(file_name, 'r', encoding = 'utf-8') as file: # Opening the file in read mode Using utf-8 encoding
+                while True:
+                    char = file.read(1) # This reads the file character by character
+                    if char.isalnum(): # Checks if the character is alphanum, until it returns false (Token is created)
+                         current_token.append(char)
+                    else:
+                        if current_token: # Checks that the list is not empty
+                            current_token = "".join(current_token).lower() # This lines joins the string & normalize the string
+                    if not char:
+                         break
     except FileNotFoundError: # I used this site https://docs.python.org/3/library/exceptions.html
         print("File doesn't exist")
+
 
     return
 
