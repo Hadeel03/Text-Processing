870fd31aeb92d1b1b5172e4253e450de0d1416d8
Ghadeer Al Jufout <ghadeer1112@yahoo.com>
2024-10-08 00:02:15 -0700

    Started writing the logic for the tokenizer

--- Text_Processing.py
+++ Text_Processing.py
@@ -1,4 +1,14 @@
 import sys
+import os # For some error checks 
+
+def tokenize(file_name): # creating a tokenizer from scratch
+    try:
+        with open(file_name, 'r'): # Opening the file in read mode
+            pass
+    except FileNotFoundError: # I used this site https://docs.python.org/3/library/exceptions.html
+        print("File doesn't exist")
+
+    return
 
 def main():
     if len(sys.argv) < 2: #Checks the number of command line arguments
@@ -7,3 +17,4 @@ def main():
     
     file_name = sys.argv[1] # This is assigns the variable file_name to the
                             # first command line arguement 
+    
